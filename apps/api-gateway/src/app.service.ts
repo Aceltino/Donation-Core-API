c2c82eadf4412b778f6abba50ec3cef4cecb010e
Aceltino <cesaltinoquianvo@gmail.com>
2026-04-23 20:13:44 -0300

    fix: adjust hello from apigateway to pass in CI test

--- apps/api-gateway/src/app.service.ts
+++ apps/api-gateway/src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      status: 'ok',
+      service: 'api-gateway',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
